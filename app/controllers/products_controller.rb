fdc6b880428a40b8d1666541efb0355ed578a2a0
Gaetan Tripelon <tripelong@gmail.com>
2023-06-13 17:10:15 +0200

    Added search bar on index.html.erb but not working yet

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,15 @@
 class ProductsController < ApplicationController
   def index
     @products = Product.all
+    if params[:query].present?
+      sql_subquery = <<~SQL
+        products.reference @@ :query
+        OR products.libelle @@ :query
+        OR products.catalogue @@ :query
+        OR products.gamme @@ :query
+      SQL
+      @products = @products.joins(:director).where(sql_subquery, query: params[:query])
+    end
   end
 
   def show
